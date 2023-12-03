e7cdaf139e7cb5a3683e1dc350d5e01b5a349f64
kardank1 <kaplun.danil.2001@gmail.com>
2023-12-03 18:04:01 +0200

    111

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function checkForSpam(message){
+    return Boolean((message.toLowerCase().indexOf("sale")+1) || (message.toLowerCase().indexOf("spam")+1));
+}
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
